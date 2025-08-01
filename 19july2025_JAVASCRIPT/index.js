8f6eded302571f305da579bed08829260dffde12
Priyanvi <priyanvigupta869@gmail.com>
2025-08-01 20:58:08 +0530

    uploading

---
+++ 19july2025_JAVASCRIPT/index.js
@@ -0,0 +1,9 @@
+console.log("Priyanvi Gupta");
+let name = null;
+let names;
+console.log(name);
+console.log(names);
+// console.log(typeof name);
+// console.log(typeof names);
+// console.log(typeof null);
+console.table([name, null, names]);
---
+++ 19july2025_JAVASCRIPT/index3.js
@@ -0,0 +1,6 @@
+// console.log(3>2);
+// console.log(null>=0);
+// console.log(null==0);
+// console.log(null<0);
+
+console.log(10 === 10);
