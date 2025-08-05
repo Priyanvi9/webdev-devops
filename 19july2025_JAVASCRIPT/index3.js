fc3577a5903466e1dd510c2e2f57d08884b36a5a
Priyanvi <priyanvigupta869@gmail.com>
2025-08-05 21:54:57 +0530

    odoeb;qojdbq;job

--- 19july2025_JAVASCRIPT/index3.js
+++ 19july2025_JAVASCRIPT/index3.js
@@ -4,3 +4,27 @@
 // console.log(null<0);
 
 console.log(10 === 10);
+const bignumber = 3743743948103810372973n;
+console.log(bignumber);
+
+console.log(typeof(bignumber));
+
+const heroes = ["shaktiman", "krish", "marvel"];
+console.log(heroes);
+console.log(typeof(heroes));
+
+let obj = {
+    names : "Priyanvi",
+    age : 19,
+    passion : "housewife",
+}
+
+console.log(obj);
+console.log(typeof(obj));
+
+
+let greet = function(){
+    console.log("GOOODD MORNING!!!!");
+}
+greet();
+console.log(typeof(greet));
